ad457f061f453cf77ecbd296835af2da82f56d78
akashgowda-09 <153371441+akashgowda-09@users.noreply.github.com>
2026-06-04 18:08:57 +0530

    Replace sensitive data with placeholder values
    Updated sensitive configuration values to placeholders for security.

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 class Config:
-    SECRET_KEY = "8f3c1a9d7e4b2c6f5a8d0e3b9c1f7a2d6e4c8b1f9a3d5c7e0b2a6d8f1c3e5a7"
+    SECRET_KEY = "CREATE YOUR OWN SECRET KEY"
 
-    MONGO_URI = "mongodb+srv://akashgowdag05_db_user:NSk41zL5IakxZccN@cluster0.0sxifv4.mongodb.net/securelocker?retryWrites=true&w=majority"
+    MONGO_URI = "YOUR MONGO-DB URL"
     DB_NAME = "private_vault"
 
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'securefolderproject@gmail.com'
-    MAIL_PASSWORD = 'nfgh elww gssa yxoo'
+    MAIL_USERNAME = 'GMAIL'
+    MAIL_PASSWORD = 'APP PASSWORD'
 
-    CLOUDINARY_CLOUD_NAME = "dnauvad1z"
-    CLOUDINARY_API_KEY = "352478634371222"
-    CLOUDINARY_API_SECRET = "dDFZCnqOobpRm2Dgl4ZXaYeJm5w"
+    CLOUDINARY_CLOUD_NAME = ""
+    CLOUDINARY_API_KEY = ""
+    CLOUDINARY_API_SECRET = ""
